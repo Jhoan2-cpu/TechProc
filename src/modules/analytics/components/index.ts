64dc1d15420d9718fcc4f2efac9716fb3705a92c
Diego-Panta <josepanta507@gmail.com>
2025-10-23 16:30:33 -0500

    Eliminando componentes de modulo analisis de datos

--- src/modules/analytics/components/index.ts
+++ src/modules/analytics/components/index.ts
@@ -1,8 +1,3 @@
-export { ProgressChart } from './ProgressChart';
-export { PieChart } from './PieChart';
-export { StatCard } from './StatCard';
-export { AnalyticsStatsCard } from './AnalyticsStatsCard';
-export { AnalyticsDashboardStats } from './AnalyticsDashboardStats';
 export { FilterSection } from './FilterSection';
 export { CourseAnalyticsCard } from './CourseAnalyticsCard';
 export { AttendanceCard } from './AttendanceCard';
